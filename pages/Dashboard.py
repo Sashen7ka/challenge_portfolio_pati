f8c9e8119b051fa3479501e18f23194bba9806b7
Sashen7ka@gmail.com <autoalexalex@gmail.com>
2022-08-21 14:04:00 +0200

    2 task changes

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -2,9 +2,14 @@ from pages.base_page import BasePage
 
 
 class Dashboard(BasePage):
- login_field_xpath = "//*[@id='login']"
- password_field_xpath = "//input[@name='password']"
- sign_in_button_xpath = "//span[@class='MuiButton-label']"
- language_button_xpath = "//div[contains(@class, 'MuiInputBase-input MuiInput-input')]"
- remind_password_hyperlink_xpath = "//child::div/a"
- scouts_panel_xpath = "//h5[text()='Scouts Panel']"
+ main_page_xpath = "//span[text()='Main page']"
+ players_xpath = "//div[contains(@class,'MuiListItemText-root jss29 jss55')]"
+ polski_xpath = "//ul[2]/div[1]/div[2]/span"
+ sign_out_xpath = "//div/ul[2]/div[2]/div[2]/span"
+ players_count_xpath = "//div[2]/div/div[1]"
+ matches_count_xpath = "//main/div[2]/div[2]/div"
+ reports_count_xpath = "//main/div[2]/div[3]/div"
+ events_count_xpath = "//main/div[2]/div[4]/div"
+ scouts_panel_xpath = "//main/div[3]/div[1]/div/div[2]"
+ dev_team_panel_xpath = "//a[contains(@href,'://app.slack.com/client/T3X4CAKNU/C3XTEGXB6')]"
+ shortcuts_xpath = "//main/div[3]/div[2]/div/div/h2"
