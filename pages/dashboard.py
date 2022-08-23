45b4815016049ecc1ff0c951b64d1f4acb6caff4
Sashen7ka@gmail.com <autoalexalex@gmail.com>
2022-08-23 23:49:25 +0200

    Task_3

---
+++ pages/dashboard.py
@@ -0,0 +1,24 @@
+import time
+from pages.base_page import BasePage
+
+
+
+class Dashboard(BasePage):
+  expected_title = "Scouts panel"
+  dashboard_url = 'https://scouts-test.futbolkolektyw.pl/en'
+  main_page_xpath = "//span[text()='Main page']"
+  players_xpath = "//div[contains(@class,'MuiListItemText-root jss29 jss55')]"
+  polski_xpath = "//ul[2]/div[1]/div[2]/span"
+  sign_out_xpath = "//div/ul[2]/div[2]/div[2]/span"
+  players_count_xpath = "//div[2]/div/div[1]"
+  matches_count_xpath = "//main/div[2]/div[2]/div"
+  reports_count_xpath = "//main/div[2]/div[3]/div"
+  events_count_xpath = "//main/div[2]/div[4]/div"
+  scouts_panel_xpath = "//main/div[3]/div[1]/div/div[2]"
+  dev_team_panel_xpath = "//a[contains(@href,'://app.slack.com/client/T3X4CAKNU/C3XTEGXB6')]"
+  shortcuts_xpath = "//main/div[3]/div[2]/div/div/h2"
+
+  def title_of_page(self):
+    time.sleep(6)
+    assert self.get_page_title(self.dashboard_url) == self.expected_title
+  pass
--- pages/login_page.py
+++ pages/login_page.py
@@ -2,11 +2,28 @@ from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
-    login_field_xpath = "//*[@id='login']"
-    password_field_xpath = "//input[@name='password']"
-    sign_in_button_xpath = "//span[@class='MuiButton-label']"
-    language_button_xpath = "//div[contains(@class, 'MuiInputBase-input MuiInput-input')]"
-    remind_password_hyperlink_xpath = "//child::div/a"
-    scouts_panel_xpath = "//h5[text()='Scouts Panel']"
-    def type_in_email(self, email):
-        self.field_send_keys(self.login_field_xpath, email)
+      login_field_xpath = "//*[@id='login']"
+      password_field_xpath = "//input[@name='password']"
+      sign_in_button_xpath = "//span[@class='MuiButton-label']"
+      login_url = ('https://scouts-test.futbolkolektyw.pl/login')
+      language_button_xpath = "//div[contains(@class, 'MuiInputBase-input MuiInput-input')]"
+      remind_password_hyperlink_xpath = "//child::div/a"
+      scouts_panel_xpath = "//h5[text()='Scouts Panel']"
+      expected_title = "Scouts panel"
+      title_of_box_xpath = "//child::div/h5"
+      header_of_box = "Scouts panel"
+
+      def type_in_email(self, email):
+        self.field_send_keys(self.login_field_xpath,email)
+
+      def type_in_password(self, password):
+        self.field_send_keys(self.password_field_xpath, password)
+
+      def click_on_the_sign_in_button(self):
+        self.click_on_the_element(self.sign_in_button_xpath)
+
+      def title_of_page(self):
+          assert self.get_page_title(self.login_url) == self.expected_title
+
+      def check_title_of_header(self):
+       self.assert_element_text(self.driver, self.title_of_box_xpath, self.header_of_box)
